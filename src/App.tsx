1850f3857188d07048d0526eab4aa97481998913
bb8dd <ldh19971027@gmail.com>
2023-03-06 23:09:00 +0900

    add: Meals

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
   );
 }
 
