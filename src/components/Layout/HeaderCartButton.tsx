b9314521880af1ea40e56b2742798dd3a79c7baf
bb8dd <ldh19971027@gmail.com>
2023-03-14 23:24:48 +0900

    add ButtonAnimation with useEffect

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartContext from '../../context/CartContext';
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
@@ -9,10 +9,24 @@ interface HeaderCartButtonProps {
 
 function HeaderCartButton({ onShowCart }: HeaderCartButtonProps) {
   const { items } = useContext(CartContext);
+  const [btnAni, setBtnAni] = useState<boolean>(false);
   const cartCount = items.length;
+  const btnAniClass = `${styles.button} ${btnAni ? styles.bump : ''}`;
+  useEffect(() => {
+    if (items.length === 0) return;
+    setBtnAni(true);
+    const timer = setTimeout(() => {
+      setBtnAni(false);
+    }, 300);
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
-    <button type="button" className={styles.button} onClick={onShowCart}>
+    <button type="button" className={btnAniClass} onClick={onShowCart}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
